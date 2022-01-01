dbc8e4add7c054e1a3f1aa0454f57a690ad96e4d
Sean D Gillespie <sean@mistersg.net>
2021-12-31 21:26:45 -0500

    Add some [very] basic error handling

---
+++ lib/Graphics/Seangine/Monad/Exception.hs
@@ -0,0 +1,16 @@
+module Graphics.Seangine.Monad.Exception where
+
+import Control.Monad.IO.Unlift (MonadIO(..))
+
+import Control.Exception
+import GHC.IO.Exception (IOErrorType(..))
+import System.IO.Error (mkIOError)
+
+systemError :: String -> IOError
+systemError str = mkIOError SystemError str Nothing Nothing
+
+throwSystemErrorIo :: String -> IO a
+throwSystemErrorIo = throwIO . systemError
+
+throwSystemError :: MonadIO m => String -> m a
+throwSystemError = liftIO . throwSystemErrorIo
