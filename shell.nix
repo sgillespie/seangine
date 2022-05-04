3001f0b5197edcfdf5eda05779db866607d1ba7d
Sean D Gillespie <sean@mistersg.net>
2022-05-04 16:09:33 -0400

    Update nix-shell support
    Pin packages to the latest version known to work, and include
    all Vulkan packages

--- shell.nix
+++ shell.nix
@@ -1,22 +1,32 @@
 {
   ghc,
-  nixPkgsVersion ? "nixos-21.11",
+  nixPkgsVersion ? "249c76eb6787420b2178a3ba4cc64c4d9c4a5997", # pin
+  nixPkgsSha256 ? "sha256:1lq2py428bfs6vv7qikzgh7hqyxw17c65dnfi35m46kjnidrrcbh",
 }:
 
-let tarball = "https://github.com/NixOS/nixpkgs/archive/${nixPkgsVersion}.tar.gz";
+let
+  tarball = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${nixPkgsVersion}.tar.gz";
+    sha256 = nixPkgsSha256;
+  };
+  nixpkgs = import tarball {};
+  inherit (nixpkgs) haskell;
 in
-with (import (fetchTarball tarball) {});
 
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "myEnv";
-  buildInputs = [
-    pkgconfig
+  buildInputs = with nixpkgs; [
     SDL2
+    glslang
+    spirv-headers
+    spirv-tools
+    vulkan-extension-layer
     vulkan-headers
     vulkan-loader
     vulkan-tools
     vulkan-tools-lunarg
     vulkan-validation-layers
+    zlib
   ];
 }
